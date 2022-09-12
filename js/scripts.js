be8537fd377153b62fbee8c61d69d95df3c0a300
Mahmoud Abdel Mohsen <ma.abdel.mohsen@gmail.com>
2022-09-12 11:45:42 +0200

    cannot select modal container :(

--- js/scripts.js
+++ js/scripts.js
@@ -34,13 +34,26 @@ let pokemonRepository = (
         // Load Pokemon details for any item of pokemon list.
         // --------------------------------------------------
         function loadDetails(pokemon) {
-            const pokemonUrl = pokemon.url;
+            console.log({pokemon})
+            let pokemonUrl = pokemon.url;
+            let detailedPokemon = pokemon;
             return fetch(pokemonUrl)
-                .then(pokemonJson => pokemonJson.json())
-                .then(
-                    pokemonData => {
+                .then(pokemonJson => {
+                    let jsonPromis = pokemonJson.json();
+                    console.log({jsonPromis})
+                    return jsonPromis;
+                })
+                .then((pokemonData) => {
+                        // let pokemonIndex = pokemonList.indexOf(pokemon);
+
+                        // pokemonList[pokemonIndex].imageUrl = pokemonData.sprites.front_default;
+                        // pokemonList[pokemonIndex].height = pokemonData.height;
+                        console.log({pokemonData});
+                        console.log(pokemon.name);
                         pokemon.imageUrl = pokemonData.sprites.front_default;
                         pokemon.height = pokemonData.height;
+                        console.log({pokemon});
+                        return pokemon
                     }
                 )
                 .catch(err => console.log(err))
@@ -80,6 +93,7 @@ let pokemonRepository = (
 
             let button = document.createElement('button');
             button.classList.add('button-class');
+            button.setAttribute('id',`${pokemon.name}`);
             button.innerText = pokemon.name;
             pokemonList.appendChild(button);
             pokemonUlList.appendChild(pokemonList);
@@ -95,8 +109,75 @@ let pokemonRepository = (
         // a function to consol log name property of an object ????
         //------------------------------------------------------
         function showDetails(pokemon) {
-            pokemonRepository.loadDetails(pokemon).then(() => console.log(pokemon.name))
+            pokemonRepository.loadDetails(pokemon).then((pokemonD) => {
+                console.log(pokemonD);
+                let modalContainer = document.querySelector('#modal-container');
+                let title = pokemonD.name;
+                console.log(title);
+                let text = ` <strong>Name :</strong> ${title} <br> <strong>Height :</strong> ${pokemon.height} meter`;
+                let img = pokemonD.imageUrl;
+  
+                function showModal(title, text, img) {
+                    modalContainer.innerHTML = '';
+                    let modal = document.createElement('div');
+                    modal.classList.add('modal');
+
+                    let closeButtonElement = document.createElement('button');
+                    closeButtonElement.classList.add('modal-close');
+                    closeButtonElement.innerText = 'Close';
+                    closeButtonElement.addEventListener('click', hideModal);
+
+                    let titleElement = document.createElement('h1');
+                    titleElement.innerText = title;
+
+                    let contentElement = document.createElement('p');
+                    contentElement.innerHTML = text;
+
+                    let imageElement = document.createElement("img");
+                    imageElement.setAttribute("src", img);
+                    imageElement.setAttribute("width", "304");
+                    imageElement.setAttribute("height", "228");
+                    imageElement.setAttribute("alt", "Pokemon Picture");
+
+                    modal.appendChild(closeButtonElement);
+                    modal.appendChild(titleElement);
+                    modal.appendChild(contentElement);
+                    modal.appendChild(imageElement);
+
+                    modalContainer.appendChild(modal);
+                    
+                    modalContainer.classList.add('is-visible');
+                }
+
+                function hideModal() {
+                    modalContainer.classList.remove('is-visible');
+                }
+
+                window.addEventListener('keydown', (e) => {
+                    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+                    hideModal();  
+                    }
+                });
+                
+                modalContainer.addEventListener('click', (e) => {
+                    // Since this is also triggered when clicking INSIDE the modal
+                    // We only want to close if the user clicks directly on the overlay
+                    let target = e.target;
+                    if (target === modalContainer) {
+                    hideModal();
+                    }
+                });
+
+                document.querySelector(`#${pokemon.name}`).addEventListener('click', () => {
+                    showModal(title, text);
+                });
+
+                // THE RETURN STATEMENT HERE
+                showModal(pokemon);
+            })
         }
+
+
 
         return {
             getall: getall,
